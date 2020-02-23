c8bf77d7ead75f33cb024dc527621ca8bc7e8916
ifreelander <roya.taleghani@gmail.com>
2020-02-23 11:32:35 +1100

    Responsive layout for 3 display size and some cleanup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React, { useState } from 'react';
 import Flexbox from 'flexbox-react';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const App = () => {
-  function myFunction() {
-    var x = document.getElementById('myNav');
-    if (x.className === 'nav') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'nav';
-    }
-  }
   return (
-    // <div className="container">
-    //   <div className="nav">
-    //     <div>
-    //       <a href="#" className="navbar">
-    //         Navbar
-    //       </a>
-    //       <a href="#">Home</a>
-    //       <a href="#">Links</a>
-    //       <a href="#">Disabled</a>
-    //       <a href="#">Dropdown</a>
-    //     </div>
-    //     <form>
-    //       <label>
-    //         <input type="text" name="Search" placeholder="Search" />
-    //       </label>
-    //       <button>Search</button>
-    //     </form>
-    //   </div>
-
-    //------creating a mobile layout for nav-----------//
-
     <div className="container">
       <div className="nav" id="myNav">
         <a href="#" className="active">
@@ -60,39 +30,41 @@ const App = () => {
           Learn more »
         </button>
       </div>
-      {/* //-----------/// */}
+
       <div className="body">
-        <h2 className="header-title-body">Heading</h2>
-        <p>
-          Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
-          malesuada magna mollis euismod. Donec sed odio dui.
-        </p>
-        <button className="body-button" href="#">
-          View details »
-        </button>
-
-        <h2 className="header-title-body">Heading</h2>
-        <p>
-          Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
-          malesuada magna mollis euismod. Donec sed odio dui.
-        </p>
-
-        <button className="body-button" href="#">
-          View details »
-        </button>
-
-        <h2 className="header-title-body">Heading</h2>
-        <p>
-          Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
-          malesuada magna mollis euismod. Donec sed odio dui.
-        </p>
-
-        <button className="body-button" href="#">
-          View details »
-        </button>
+        <div className="body-div">
+          <h2 className="header-title-body">Heading</h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+            commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
+            porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <button className="body-button" href="#">
+            View details »
+          </button>
+        </div>
+        <div className="body-div">
+          <h2 className="header-title-body">Heading</h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+            commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
+            porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <button className="body-button" href="#">
+            View details »
+          </button>
+        </div>
+        <div className="body-div">
+          <h2 className="header-title-body">Heading</h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+            commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
+            porta sem malesuada magna mollis euismod. Donec sed odio dui.
+          </p>
+          <button className="body-button" href="#">
+            View details »
+          </button>
+        </div>
       </div>
       <hr className="hr" />
 
